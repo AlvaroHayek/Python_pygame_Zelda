f533b4f3631911c213a0a925beeb9390941a8157
HayekAlv <125888859+AlvaroHayek@users.noreply.github.com>
2025-07-15 22:21:19 +0200

    Update level.py

--- code/level.py
+++ code/level.py
@@ -6,6 +6,7 @@ class Level:
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
         
+        
     def run(self):
         # update and draw the game
         pass
