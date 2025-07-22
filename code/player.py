0368694e9af2f8f11e621a3e3dfeffa3cee7fab3
HayekAlv <125888859+AlvaroHayek@users.noreply.github.com>
2025-07-22 15:51:55 +0200

    Update player.py

--- code/player.py
+++ code/player.py
@@ -24,3 +24,12 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load('../graphics/NinjaAdventure/Backgrounds/ZeldaTiles/Player_cropped.png').convert_alpha()
         self.rect = self.crop_image.get_rect(topleft = pos)
         
+        self.direction = pygame.math.Vector2()
+        
+    def input(self):
+        keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        if keys[pygame.K_DOWN]:
+            self.direction.y = 1
