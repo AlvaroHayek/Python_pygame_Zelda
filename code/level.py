f35f26eeb8b8eadcf2c0539e32bbe71a1e58c97a
HayekAlv <125888859+AlvaroHayek@users.noreply.github.com>
2025-07-24 18:23:21 +0200

    tilemap

--- code/level.py
+++ code/level.py
@@ -44,11 +44,19 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
         
+        # creating the floor
+        floor_save_path = '../graphics/NinjaAdventure/Backgrounds/ZeldaTiles/tilemap.png'
+        self.floor_surf = pygame.image.load(floor_save_path)
+        self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
+        
+        
     def custom_draw(self, player):
         
         # getting offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+        
+        #drawing the floor
         
         #for sprite in self.sprites():
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
