ea20a11797b2d43553613e78383cb631fc3c7990
HayekAlv <125888859+AlvaroHayek@users.noreply.github.com>
2025-07-25 17:12:25 +0200

    water fixed

--- code/water.py
+++ code/water.py
@@ -1,5 +1,6 @@
 import os
 import pygame
+from PIL import Image
 from settings import *
 
 class Water(pygame.sprite.Sprite):
@@ -7,7 +8,13 @@ class Water(pygame.sprite.Sprite):
         super().__init__(groups)
         #self.sprite_type = sprite_type
         #self.image = surface
-        self.image = pygame.image.load('../graphics/NinjaAdventure/Backgrounds/Tilesets/TilesetWater.png').convert_alpha()
+        
+        def clean_and_load_png(filename, png_path):
+            img = Image.open(png_path)
+            img.save(filename, icc_profile=None)
+            return pygame.image.load(filename).convert_alpha()
+        
+        self.image = clean_and_load_png('TilesetWater2.png','../graphics/NinjaAdventure/Backgrounds/Tilesets/TilesetWater.png')
         #self.rect = self.image.get_rect(topleft = pos)
         self.original_size = self.image.get_size()
         new_size = (self.original_size[0] * 2.8, self.original_size[1] * 2.8)
@@ -25,4 +32,5 @@ class Water(pygame.sprite.Sprite):
         self.image = pygame.image.load('../graphics/NinjaAdventure/Backgrounds/ZeldaTiles/Water_cropped.png').convert_alpha()
         self.rect = self.crop_image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0,-10)
-        
+    
+       
