840055e7c73308356ee96a1db5ef03b675c38b4d
HayekAlv <125888859+AlvaroHayek@users.noreply.github.com>
2025-08-01 02:05:01 +0200

    implementing ui

--- code/level.py
+++ code/level.py
@@ -6,6 +6,7 @@ from water import Water
 from debug import debug
 from weapon import Weapon
 from random import choice
+from ui import UI
 
 class Level:
     def __init__(self):
@@ -22,6 +23,10 @@ class Level:
         
         # sprite setup
         self.create_map()
+        
+        # user interface
+        self.ui = UI()
+        
         
     def create_map(self):
         tile_save_path = "../graphics/NinjaAdventure/Backgrounds/ZeldaTiles/Rock_cropped.png"
@@ -57,7 +62,8 @@ class Level:
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.status)
+        #debug(self.player.status)
+        self.ui.display(self.player)
         
 class YSortCameraGroup(pygame.sprite.Group):
     def __init__(self):
