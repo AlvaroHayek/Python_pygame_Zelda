f2dfe9adcb3bce0ff6783c5c90346916bc0bd168
HayekAlv <125888859+AlvaroHayek@users.noreply.github.com>
2025-08-01 02:16:27 +0200

    Update settings.py

--- code/settings.py
+++ code/settings.py
@@ -13,9 +13,15 @@ UI_FONT = '../graphics/NinjaAdventure/Ui/Font/NormalFont.ttf'
 UI_FONT_SIZE = 18
 
 # general colors
-
+WATER_COLOR = '#71ddee'
+UI_BG_COLOR = '#222222'
+UI_BORDER_COLOR = '#111111'
+TEXT_COLOR = '#EEEEEE'
 
 # ui colors
+HEALTH_COLOR = 'red'
+ENERGY_COLOR = 'blue'
+UI_BORDER_COLOR_ACTIVE = 'gold'
 
 # weapons
 weapon_data = {
