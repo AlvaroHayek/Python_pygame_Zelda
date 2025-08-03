c40e5caf82babd1fa35f6d0603b266ec0e31cbdb
HayekAlv <125888859+AlvaroHayek@users.noreply.github.com>
2025-08-03 21:47:34 +0200

    Update enemy.py

--- code/enemy.py
+++ code/enemy.py
@@ -10,5 +10,10 @@ class Enemy(Entity):
         self.sprite_type = 'enemy'
         
         # graphics setup
+        self.import_graphics(monster_name)
         self.image = pygame.Surface((64,64))
         self.rect = self.image.get_rect(topleft=pos)
+        
+    def import_graphics(self,name):
+        self.animations = {'idle':[],'move':[],'attack':[]}
+        main = f'../graphics/NinjaAdventure/Actor/Monsters/{name}'
