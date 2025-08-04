ac1a2e46511a3839a5562d6e125942d86b623c27
HayekAlv <125888859+AlvaroHayek@users.noreply.github.com>
2025-08-04 17:46:17 +0200

    Update level.py

--- code/level.py
+++ code/level.py
@@ -73,12 +73,21 @@ class Level:
             self.current_attack.kill()
         self.current_attack = None
     
+    def player_attack_logic(self):
+        if self.attack_sprites:
+            for attack_sprite in self.attack_sprites:
+                collision_sprites = pygame.sprite.spritecollide(attack_sprite,self.attackable_sprites,True)
+                if collision_sprites:
+                    for target_sprite in collision_sprites:
+                        target_sprite.kill()
+    
     def run(self):
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
         #debug(self.player.status)
         self.visible_sprites.enemy_update(self.player)
+        self.player_attack_logic()
         self.ui.display(self.player)
         
 class YSortCameraGroup(pygame.sprite.Group):
