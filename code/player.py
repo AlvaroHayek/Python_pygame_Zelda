1b776dee26a23fffc503329040cf60a7e4db4eb6
HayekAlv <125888859+AlvaroHayek@users.noreply.github.com>
2025-08-08 17:58:49 +0200

    Update player.py

--- code/player.py
+++ code/player.py
@@ -207,6 +207,12 @@ class Player(Entity):
         weapon_damage = weapon_data[self.weapon]['damage']
         return base_damage + weapon_damage
     
+    def energy_recovery(self):
+        if self.energy <= self.stats['energy']:
+            self.energy += 1
+        else:
+            self.energy = self.stats['energy']
+        
     def update(self):
         self.input()
         self.cooldowns()
