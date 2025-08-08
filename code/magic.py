180c7fb891d5243aa4600257fb3e059d86389780
HayekAlv <125888859+AlvaroHayek@users.noreply.github.com>
2025-08-08 18:23:50 +0200

    more magic

--- code/magic.py
+++ code/magic.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import *
+from random import randint
 
 class MagicPlayer:
     def __init__(self,animation_player):
@@ -19,15 +20,15 @@ class MagicPlayer:
         if player.energy >= cost:
             player.energy -= cost
             
-        if player.stats.split('_')[0] == 'right': direction = pygame.math.Vector2(1,0)
-        elif player.stats.split('_')[0] == 'left': direction = pygame.math.Vector2(-1,0)
-        elif player.stats.split('_')[0] == 'up': direction = pygame.math.Vector2(0,-1)
+        if player.status.split('_')[0] == 'right': direction = pygame.math.Vector2(1,0)
+        elif player.status.split('_')[0] == 'left': direction = pygame.math.Vector2(-1,0)
+        elif player.status.split('_')[0] == 'up': direction = pygame.math.Vector2(0,-1)
         else: direction = pygame.math.Vector2(0,1)
         
         for i in range(1,6):
             if direction.x: #horizontal
                 offset_x = (direction.x * i) * TILESIZE
-                x = player.rect.centerx + offset_x
+                x = player.rect.centerx + offset_x + randint(-TILESIZE //3, TILESIZE //3)
                 y = player.rect.centery
                 self.animation_player.create_particles('flame', (x,y),groups)
             else: #vertical
