fc79031d8a8367ce957755ba1038d8168e33c257
HayekAlv <125888859+AlvaroHayek@users.noreply.github.com>
2025-08-09 18:47:53 +0200

    Update enemy.py

--- code/enemy.py
+++ code/enemy.py
@@ -3,7 +3,7 @@ from settings import *
 from entity import Entity
 
 class Enemy(Entity):
-    def __init__(self,monster_name,pos,groups,obstacle_sprites,damage_player,trigger_death_particles):
+    def __init__(self,monster_name,pos,groups,obstacle_sprites,damage_player,trigger_death_particles,add_exp):
         
         # general setup
         super().__init__(groups)
@@ -44,6 +44,7 @@ class Enemy(Entity):
         self.attack_cooldown = 400
         self.damage_player = damage_player
         self.trigger_death_particles = trigger_death_particles
+        self.add_exp = add_exp
         
         # invincibility timer
         self.vulnerable = True
@@ -149,6 +150,7 @@ class Enemy(Entity):
         if self.health <= 0:
             self.kill()
             self.trigger_death_particles(self.rect.center,self.monster_name)
+            self.add_exp(self.exp)
             
     def hit_reaction(self):
         if not self.vulnerable:
